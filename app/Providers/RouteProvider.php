1b371100777694b434a3ad2a354801d19d198c84
Radu <radu.bordei89@gmail.com>
2016-03-03 16:58:05 +0200

    adding climate and template provider.

--- app/Providers/RouteProvider.php
+++ app/Providers/RouteProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services;
+namespace App\Providers;
 
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
---
+++ app/Providers/TemplateProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use League\Plates\Engine;
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+class TemplateProvider implements ServiceProviderInterface {
+
+    /**
+     * Registers services on the given container.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     *
+     * @param Container $pimple A container instance
+     * @return $this
+     */
+    public function register(Container $pimple) {
+        $view = $pimple['settings']->get('view', []);
+
+        $engine = new Engine(
+            array_pop($view['paths'])
+        );
+
+        foreach($view['paths'] as $alias =>  $path)
+            $engine->addFolder($alias, $path);
+
+        ioc('view', $engine);
+
+        return $this;
+    }
+}

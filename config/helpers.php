e086e07deeaab2949f64706dd85f734e64bc1eef
Radu <radu.bordei89@gmail.com>
2016-03-03 11:34:19 +0200

    adding some helpers methods.

--- config/helpers.php
+++ config/helpers.php
@@ -1,2 +1,71 @@
 <?php
 
+/**
+ * App function .
+ *
+ * @param null $key
+ * @param null $value
+ * @return mixed
+ */
+function ioc($key = null, $value = null) {
+    $app = App\App::getInstance();
+
+    if( is_null($key) )
+        return $app;
+
+    if( is_null($value) )
+        return $app->getContainer()->get($key);
+
+    return $app->getContainer()[$key] = $value;
+}
+
+/**
+ * Get instance App
+ *
+ * @param null $method
+ * @return \App\App
+ */
+function app($method = null) {
+    if(! is_null($method))
+        return App\App::getInstance()->{$method}();
+
+    return App\App::getInstance();
+}
+
+/**
+ * Get path .
+ *
+ * @param $path
+ * @return mixed
+ */
+function path($path) {
+    return ioc('paths')[$path];
+}
+
+/**
+ * Get app path .
+ *
+ * @return mixed
+ */
+function app_path() {
+    return path('app');
+}
+
+/**
+ * Get config path
+ *
+ * @return mixed
+ */
+function config_path() {
+    return path('config');
+}
+
+/**
+ * Get public path .
+ *
+ * @return mixed
+ */
+function public_path() {
+    return path('public');
+}
+

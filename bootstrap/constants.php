48bbda179ba4129985776fdecb3564e265930626
Radu <radu.bordei89@gmail.com>
2016-03-03 11:33:40 +0200

    delete abstaractProvider, constants file.

--- bootstrap/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-foreach (array('bootstrap', 'config', 'app', 'pubic') as $path) {
-    $path_name = strtoupper($path . '_PATH');
-
-    if(! defined($path_name))
-        define($path_name, __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . $path . DIRECTORY_SEPARATOR);
-}

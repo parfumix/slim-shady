4dd294168cc1ae3a0546719f5b21b1da2b1f450f
Radu <radu.bordei89@gmail.com>
2016-03-04 20:59:45 +0200

    adding routes and layouts.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,7 +1,22 @@
 <?php
+
 $app = app();
 
-$app->get('/hello/{name}', function ($request, $response, $args) {
-    return $response->write("Hello " . $args['name']);
+$app->get('/', function (\Slim\Http\Request $request, $response, $args) {
+
 });
 
+$app->group('/u', function () {
+    $this->get('/reset-password', function ($request, $response, $args) {
+
+    })->setName('user-password-reset');
+
+    $this->get('/login', function ($request, $response, $args) {
+        return $response->write(view('user/login')->render());
+
+    })->setName('user-login');
+
+    $this->get('/register', function ($request, $response, $args) {
+
+    })->setName('user-register');
+});

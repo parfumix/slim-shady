96f8041315297dd26c5dbafd2afc2d6745a25ef7
Radu <radu.bordei89@gmail.com>
2016-03-09 11:05:34 +0200

    adding additional fields.

--- app/Entity/User.php
+++ app/Entity/User.php
@@ -8,9 +8,16 @@ class User extends Entity {
 
     protected static $table = 'users';
 
+    /**
+     * Get fields .
+     *
+     * @return array
+     */
     public static function fields() {
         return [
-            'id' => ['type' => 'integer', 'primary' => true, 'autoincrement' => true]
+            'id' => ['type' => 'integer', 'primary' => true, 'autoincrement' => true],
+            'email' => ['type' => 'string', 'required' => false],
+            'password' => ['type' => 'string', 'required' => false],
         ];
     }
 }

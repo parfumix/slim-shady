dbb82f8f8aff351204f1b49be7a270fa74ca434c
Radu <radu.bordei89@gmail.com>
2016-03-10 12:17:41 +0200

    time generated.

--- public/index.php
+++ public/index.php
@@ -4,4 +4,11 @@ declare(strict_types=1);
 
 require_once '../vendor/autoload.php';
 
+$bench = (new Ubench);
+$bench->start();
+
 require_once '../bootstrap/app.php';
+
+$bench->end();
+
+echo '<script>console.log("Generated in '.$bench->getTime().'. Memory usage '.$bench->getMemoryUsage().'")</script>';

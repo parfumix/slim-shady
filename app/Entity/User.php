b41f837fed0dc569bb2bd0885b6b975606a61e0d
Radu <radu.bordei89@gmail.com>
2016-03-10 17:24:33 +0200

    set up relations.

--- app/Entity/User.php
+++ app/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use Spot\Entity;
+use Spot\EntityInterface;
+use Spot\MapperInterface;
 
 class User extends Entity {
 
@@ -18,6 +20,19 @@ class User extends Entity {
             'id' => ['type' => 'integer', 'primary' => true, 'autoincrement' => true],
             'email' => ['type' => 'string', 'required' => false],
             'password' => ['type' => 'string', 'required' => false],
+        ];
+    }
+
+    /**
+     * Relations setUp
+     *
+     * @param MapperInterface $mapper
+     * @param EntityInterface $entity
+     * @return array
+     */
+    public static function relations(MapperInterface $mapper, EntityInterface $entity) {
+        return [
+            'socials' => $mapper->hasMany($entity, User::class, 'user_id')
         ];
     }
 }

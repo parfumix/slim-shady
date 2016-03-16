aad5c42d933ada98dd5c43dad107efeaae374fae
Radu <radu.bordei89@gmail.com>
2016-03-16 17:39:28 +0200

    adding kernel folder.

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 require_once '../vendor/autoload.php';
 
@@ -11,4 +11,4 @@ require_once '../bootstrap/app.php';
 
 $bench->end();
 
-echo '<script>console.log("Generated in '.$bench->getTime().'. Memory usage '.$bench->getMemoryUsage().'")</script>';
+echo '<script>console.log("Generated in ' . $bench->getTime() . '. Memory usage ' . $bench->getMemoryUsage() . '")</script>';
